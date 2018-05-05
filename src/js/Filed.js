330c511ea3c2ec3c5cfa5560a3b72902819754be
313183373 <313183373@qq.com>
2018-05-05 12:26:21 +0800

    v1

--- src/js/Filed.js
+++ src/js/Filed.js
@@ -1,22 +1,50 @@
 class Field {
-    constructor(name, type) {
-        this.name = name;
+    constructor(type) {
+        this.name = type === 'text' ? '请输入字段' : '请输入日期';
         this.type = type;
     }
 
 
-    toDOM(){
-        const d = $('<div>',{
-            class:'field',
+    toDOM() {
+        const d = $('<div>', {
+            class: 'field',
         });
-        
-        $('<h1>',{
-            text:this.name
+
+        const h = $('<h3>', {
+            text: this.name,
+            contenteditable: true,
         }).appendTo(d);
 
-        $('<input>',{
-            type:this.type,
+        h.on('input', () => {
+            this.name = h.text();
+        })
+
+        const i = $('<input>', {
+            type: 'text',
+            'data-type': this.type,
         }).appendTo(d);
+
+        const b = $('<button>', {
+            text: '删除'
+        }).appendTo(d);
+
+        b.click(function(){
+            d.remove();
+        })
+
+        b.hide();
+
+        d.hover(function(){
+            b.show();
+        },function(){
+            b.hide();
+        })
+
+        if (i.data('type') === 'date') {
+            i.datepicker({
+                showAnim: 'slideDown'
+            });
+        }
 
         return d;
     }
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
 $(function () {
     const addButton = $('#addButton');
+    const previewButton = $('#preview');
     const dialog = $('#dialog');
     const content = $('#content');
+
+    // content.sortable(); 
+    // content.selectable();
+    // content.disableSelection();
 
     const fieldList = [];
 
@@ -18,7 +23,13 @@ $(function () {
         modal:true,
         buttons: {
             "提交":function(){
-                content.append(new Field('你的名字叫什么?','text').toDOM());
+                let type = 'text';
+                $(this).find('input').each(function(){
+                    type = $(this).prop('checked')?$(this).val():type;
+                })
+                const f = new Field(type);
+                content.append(f.toDOM());
+                fieldList.push(f);
                 dialog.dialog('close');
             },
             "取消":function(){
@@ -26,7 +37,33 @@ $(function () {
             }
         }
     });
+
     addButton.click(function () {
+        dialog.children()[0].checked = true;
         dialog.dialog('open');
     });
+
+    previewButton.click(function(){
+        const that = $(this);
+        if(that.text() === '预览'){
+            addButton.hide();
+            that.text('编辑');
+            $('h3').prop('contenteditable',false);
+        }else{
+            addButton.show();
+            that.text('预览');
+            $('h3').prop('contenteditable',true);
+        }
+        $('#content').toggleClass('disable');
+    })
 });
+
+// 快捷键
+window.onkeypress = function(e){
+    if(e.key === 'f' && e.ctrlKey){
+        addButton.click();
+    }
+    if(e.key === 'v' && e.ctrlKey){
+        previewButton.click();
+    }
+}
